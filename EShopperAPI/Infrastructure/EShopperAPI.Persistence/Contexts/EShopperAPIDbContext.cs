fdde3c9e78414bb471f3d00e34a9f24d9a67d51b
Egemen <egementosyalitr@gmail.com>
2022-12-24 20:48:51 +0300

    Add database context

---
+++ EShopperAPI/Infrastructure/EShopperAPI.Persistence/Contexts/EShopperAPIDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EShopperAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopperAPI.Persistence.Contexts
+{
+    public class EShopperAPIDbContext : DbContext
+    {
+        public EShopperAPIDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
