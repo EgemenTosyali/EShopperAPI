ab361a9b8e24fb9481bcf63069bd74711ad5cedd
Egemen <egementosyalitr@gmail.com>
2022-12-24 22:04:32 +0300

    Add DesignTimeDbContextFactory

---
+++ EShopperAPI/Infrastructure/EShopperAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using EShopperAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopperAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<EShopperAPIDbContext>
+    {
+        public EShopperAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<EShopperAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
