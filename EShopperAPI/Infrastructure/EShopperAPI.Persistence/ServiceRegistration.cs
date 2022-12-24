70f3da2fce983b9d8544012bd872707002af1f29
Egemen <egementosyalitr@gmail.com>
2022-12-24 21:06:34 +0300

    Add connection string & configuration

--- EShopperAPI/Infrastructure/EShopperAPI.Persistence/ServiceRegistration.cs
+++ EShopperAPI/Infrastructure/EShopperAPI.Persistence/ServiceRegistration.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using EShopperAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +14,7 @@ namespace EShopperAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection service)
         {
-            
+            service.AddDbContext<EShopperAPIDbContext>(option => option.UseNpgsql(Configuration.ConnectionString));
         }
     }
 }
