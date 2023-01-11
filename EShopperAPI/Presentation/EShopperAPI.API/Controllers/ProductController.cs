a12dbd441b50e2916f3823faf8d14a6fa601b762
Egemen Tosyalı <egementosyalitr@gmail.com>
2023-01-12 01:37:43 +0300

    Add post method
    Post method for Products (test)

---
+++ EShopperAPI/Presentation/EShopperAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using EShopperAPI.Application.Repositories;
+using EShopperAPI.Application.ViewModels.Products;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EShopperAPI.API.Controllers
+{
+    public class ProductController : Controller
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+
+        public ProductController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProduct_ViewModel model)
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock
+            });
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+    }
+}
