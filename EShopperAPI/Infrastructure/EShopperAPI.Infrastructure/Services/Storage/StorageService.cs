2f11289ac714c044259c9f7db66a331f4b291e19
Egemen Tosyalı <egementosyalitr@gmail.com>
2023-01-30 16:01:48 +0300

    Add storage interfaces and concrete

---
+++ EShopperAPI/Infrastructure/EShopperAPI.Infrastructure/Services/Storage/StorageService.cs
@@ -0,0 +1,33 @@
+﻿using EShopperAPI.Application.Abstractions.Storage;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopperAPI.Infrastructure.Services.Storage
+{
+    public class StorageService : IStorageService
+    {
+        private IStorage _storage;
+
+        public StorageService(IStorage storage)
+        {
+            _storage = storage;
+        }
+        public string StorageName { get => _storage.GetType().Name; }
+
+        public async Task DeleteAsync(string path, string fileName)
+            => await _storage.DeleteAsync(path, fileName);
+
+        public List<string> GetFiles(string path)
+            => _storage.GetFiles(path);
+
+        public bool HasFile(string path, string fileName)
+            => _storage.HasFile(path, fileName);
+
+        public Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
+            => _storage.UploadAsync(path, files);
+    }
+}
