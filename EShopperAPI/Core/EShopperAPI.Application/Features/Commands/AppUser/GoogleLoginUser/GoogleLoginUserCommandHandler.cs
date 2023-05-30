f9a0ee323e1446252b218e622b86e7437c783740
Egemen Tosyalı <egementosyalitr@gmail.com>
2023-05-31 00:30:59 +0300

    refactor: google login is no longer depends on persistence layer

--- EShopperAPI/Core/EShopperAPI.Application/Features/Commands/AppUser/GoogleLoginUser/GoogleLoginUserCommandHandler.cs
+++ EShopperAPI/Core/EShopperAPI.Application/Features/Commands/AppUser/GoogleLoginUser/GoogleLoginUserCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using EShopperAPI.Application.Abstractions.Token;
+﻿using EShopperAPI.Application.Abstractions.Services.Authentication;
+using EShopperAPI.Application.Abstractions.Token;
 using EShopperAPI.Application.DTOs;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
@@ -7,55 +8,20 @@ namespace EShopperAPI.Application.Features.Commands.AppUser.GoogleLoginUser
 {
     public class GoogleLoginUserCommandHandler : IRequestHandler<GoogleLoginUserCommandRequest, GoogleLoginUserCommandResponse>
     {
-        readonly UserManager<EShopperAPI.Domain.Entities.Identities.AppUser> _userManager;
-        readonly ITokenHandler _tokenHandler;
+        readonly IExternalAuthentication _authService;
 
-        public GoogleLoginUserCommandHandler(UserManager<Domain.Entities.Identities.AppUser> userManager, ITokenHandler tokenHandler)
+        public GoogleLoginUserCommandHandler(IExternalAuthentication authService)
         {
-            _userManager = userManager;
-            _tokenHandler = tokenHandler;
+            _authService = authService;
         }
 
         public async Task<GoogleLoginUserCommandResponse> Handle(GoogleLoginUserCommandRequest request, CancellationToken cancellationToken)
         {
-            return null;
-
-            //var settings = new GoogleJsonWebSignature.ValidationSettings()
-            //{
-            //    Audience = new List<string> { "679370732223-pjg7iiarc39citrcq8gvf3k2kblmteni.apps.googleusercontent.com" }
-            //};
-            //var payload = await GoogleJsonWebSignature.ValidateAsync(request.idToken);
-            //var info = new UserLoginInfo(request.provider, payload.Subject, request.provider);
-            //Domain.Entities.Identities.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
-
-            //bool result = user != null;
-            //if (user == null)
-            //{
-            //    user = await _userManager.FindByEmailAsync(payload.Email);
-            //    if (user == null)
-            //    {
-            //        user = new()
-            //        {
-            //            Id = Guid.NewGuid().ToString(),
-            //            Email = payload.Email,
-            //            UserName = payload.Email,
-            //            Name = payload.Name,
-            //            SurName = payload.Name
-            //        };
-            //    }
-            //    var identityResult = await _userManager.CreateAsync(user);
-            //    result = identityResult.Succeeded;
-            //}
-            //if (result)
-            //    await _userManager.AddLoginAsync(user, info);
-            //else
-            //    throw new Exception("Google Authentication failed");
-
-            //Token token = _tokenHandler.CreateAccessToken(5);
-            //return new()
-            //{
-            //    token = token
-            //};
+            var token = await _authService.GoogleLoginAsync(request.idToken, 15);
+            return new()
+            {
+                token = token
+            };
         }
     }
 }
--- EShopperAPI/Infrastructure/EShopperAPI.Persistence/Services/AuthService.cs
+++ EShopperAPI/Infrastructure/EShopperAPI.Persistence/Services/AuthService.cs
@@ -4,8 +4,10 @@ using EShopperAPI.Application.DTOs;
 using EShopperAPI.Application.Exceptions;
 using EShopperAPI.Application.Features.Commands.AppUser.LoginUser;
 using EShopperAPI.Domain.Entities.Identities;
+using Google.Apis.Auth;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,17 +21,55 @@ namespace EShopperAPI.Persistence.Services
         private UserManager<AppUser> _userManager;
         private SignInManager<AppUser> _signInManager;
         private ITokenHandler _tokenHandler;
+        private IConfiguration _configuration;
 
-        public AuthService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenHandler tokenHandler)
+        public AuthService(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ITokenHandler tokenHandler, IConfiguration configuration)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _tokenHandler = tokenHandler;
+            _configuration = configuration;
         }
-
-        public Task<Token> GoogleLoginAsync(string idToken, int accessTokenLifeTime)
+        async Task<Token> CreateUserExternalAsync(AppUser user, string email, string name, UserLoginInfo info, int accessTokenLifeTime)
         {
-            throw new NotImplementedException();
+            bool result = user != null;
+            if (user == null)
+            {
+                user = await _userManager.FindByEmailAsync(email);
+                if (user == null)
+                {
+                    user = new()
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = email,
+                        UserName = email,
+                        Name = name,
+                        SurName = name
+                    };
+                    var identityResult = await _userManager.CreateAsync(user);
+                    result = identityResult.Succeeded;
+                }
+            }
+            if (result)
+                await _userManager.AddLoginAsync(user, info);
+            else
+                throw new Exception("Invalid auth");
+
+            Token token = _tokenHandler.CreateAccessToken(accessTokenLifeTime);
+            return token;
+        }
+        public async Task<Token> GoogleLoginAsync(string idToken, int accessTokenLifeTime)
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string> { _configuration["ExternalLoginConfigs:Google:Audience"] }
+            };
+            var payload = await GoogleJsonWebSignature.ValidateAsync(idToken);
+            var info = new UserLoginInfo("GOOGLE", payload.Subject, "GOOGLE");
+
+            AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+            return await CreateUserExternalAsync(user, payload.Email, payload.Name, info, accessTokenLifeTime);
         }
 
         public async Task<Token> LoginAsync(string usernameOrEmail, string password, int accessTokenLifeTime)
